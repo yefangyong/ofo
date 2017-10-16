23a4f50663a5e5fc6fb4a1f4240f48e5cc2dfeb3
yefy <1281074511@qq.com>
2017-10-16 14:04:12 +0800

    腾讯地图的相关配置

---
+++ application/extra/tx.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yefy
+ * Date: 2017/10/16
+ * Time: 11:05
+ */
+/**
+ *
+ * 腾讯地图的相关配置
+ */
+return [
+    'tx_url'=>'http://apis.map.qq.com/ws/geocoder/v1/',
+    'key'=>'CKYBZ-ED2CX-XHM4T-7TSWJ-B6RZO-RMB7H'
+];
